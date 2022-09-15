806b25db0046fd56e562b9bbf6796c5c2b332156
trnwrckd <trnwrckd@gmail.com>
2022-09-15 18:49:35 +0600

    added update and delete endpoints

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// uri and client
+// uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2w1ht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+// client
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // run
@@ -30,7 +31,7 @@ async function run() {
       res.json(folders)
     })
 
-    // get folders for parent directory
+    // get children folders by parent id
     app.get("/folders/:id", async (req, res) => {
       const id = req.params.id
       const query = { parent: id }
@@ -43,9 +44,9 @@ async function run() {
     // get details of folder by id
     app.get("/folderDetails/:id", async (req, res) => {
       const id = req.params.id
-      console.log(id)
       let query, cursor, folder
 
+      // if id is -1, return My Drive as root
       if (id == "-1") {
         folder = {
           _id: "-1",
@@ -69,8 +70,38 @@ async function run() {
     })
 
     // delete folder and children if exists
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const parent = await foldersCollection.deleteOne(query) // remove parent
+      // if children exist, remove
+      const result = await foldersCollection.deleteMany({ "ancestors.id": id })
+      // return toal deleted count
+      result.deletedCount = parent.deletedCount + result.deletedCount
+
+      res.json(result)
+    })
 
     // update folder name
+    app.put("/update/:id", async (req, res) => {
+      const folder = req.body
+      const id = folder._id
+
+      const filter = { _id: ObjectId(id) }
+      const options = { upsert: true }
+      const updateDoc = { $set: { name: folder.name } }
+      // update parent
+      const parent = await foldersCollection.updateOne(filter, updateDoc, options)
+
+      // if children exist, update them
+      const childrenFilter = { "ancestors.id": id }
+      const updateChildren = { $set: { "ancestors.$.name": folder.name } }
+      const result = await foldersCollection.updateMany(childrenFilter, updateChildren, options)
+
+      // return total modified count
+      result.modifiedCount = parent.modifiedCount + result.modifiedCount
+      res.json(result)
+    })
   } finally {
   }
 }
@@ -79,7 +110,7 @@ run().catch(console.dir)
 
 app.get("/", (req, res) => {
   console.log("Server started")
-  res.send("Server running")
+  res.send("Google Drive clone : Server running")
 })
 
 app.listen(port, () => {
